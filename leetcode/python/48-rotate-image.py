3196ecab2e618ef6eb334d2c9d0992047c567474
hassaanabid <abid.hassaan@outlook.com>
2020-08-10 20:06:33 -0400

    minor

--- leetcode/python/48-rotate-image.py
+++ leetcode/python/48-rotate-image.py
@@ -36,6 +36,35 @@ class Solution:
         2. tranpose matrix
     """
         
+class Solution:
+    """
+        first row = 0, last row = n - 1
+        
+        90 degree clockwise rotation:
+            row 0 -> col n-1
+            row 1 -> col n-2
+            ...
+            row n-2 -> col 1
+            row n-1 -> col 0
+        
+        # transpose of a matrix is a new matrix whose rows are the columns of the original
+        # equvivalently: transpose of a matrix is an operator which flips a matrix over its diagonal
+        tranpose of a matrix:
+            row 0 -> col 0
+            row 1 -> col 1
+            ...
+            row n-2 -> col n-2
+            row n-1 -> col n-1
+        
+        hence, tranpose yields partial transformation
+            reverse each row for full transformation
+            i.e. col 0 -> col n-1 etc
+        
+        -> 90 degree anti-clockwise rotation
+        1. rerverse rows
+        2. tranpose matrix
+    """
+        
     def rotate(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
@@ -45,7 +74,7 @@ class Solution:
             lo = (n-l) // 2 # start index of layer
             hi = lo + l # end index of layer
             for i in range(l):
-                # indicies for 4-way swap
+                # indices for 4-way swap
                 top_left, top_right, bottom_right, bottom_left = \
                 (lo, lo + i), (lo + i, hi), (hi, hi - i), (hi - i, lo)
                 """
@@ -63,17 +92,26 @@ class Solution:
                 matrix[top_right[0]][top_right[1]]       = tmp
 
                 
-    def rotate(self, matrix: List[List[int]]) -> None:
+    def rotate_2(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        
+        self.transpose_n_by_n(matrix)
         n=len(matrix)
-
+        for i in range(n):
+            # matrix[i].reverse()
+            self.my_reverse(matrix[i])
+            
+    
+    def my_reverse(self, arr):
+        n = len(arr)
+        for i in range(n//2):
+            arr[i], arr[n-1-i] = arr[n-1-i], arr[i]
+    
+    def transpose_n_by_n(self, matrix):
+        """ in-place tranpose a n by n matrix"""
+        n=len(matrix)
         # loop over top-right half of diagonal and swap it with bottom-left half
         for i in range(n):
             for j in range(i,n):
                 matrix[i][j], matrix[j][i]= matrix[j][i], matrix[i][j]
-                
-        for i in range(n):
-            matrix[i].reverse()
